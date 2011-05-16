d6fe09600fc2f90fa1f73b7321ad27821f878305
Rune Madsen <runemadsen@AxelMusics-Mac-Pro.local>
2011-05-16 15:31:38 -0400

    Initial Commit

---
+++ ColorKit/ColorKitAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  ColorKitAppDelegate.h
+//  ColorKit
+//
+//  Created by Rune Madsen on 11-05-16.
+//  Copyright 2011 Lakeshore Distribution Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ColorKitAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ ColorKit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ColorKit
+//
+//  Created by Rune Madsen on 11-05-16.
+//  Copyright 2011 Lakeshore Distribution Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	int retVal = UIApplicationMain(argc, argv, nil, nil);
+	[pool release];
+	return retVal;
+}
